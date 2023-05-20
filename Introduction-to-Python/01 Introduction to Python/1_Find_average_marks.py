3c7a3cd9ae47a7aae15b6fa5d80818340337942a
Ankur Kumar Pathak <98033479+AnkurPath@users.noreply.github.com>
2023-05-20 20:58:31 +0530

    Create 1_Find_average_marks.py

---
+++ Introduction-to-Python/01 Introduction to Python/1_Find_average_marks.py
@@ -0,0 +1,29 @@
+# Find average Marks
+# Send Feedback
+# Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+# Input format :
+# 3 Test marks (in different lines)
+# Output format :
+# Average 
+# Sample Input 1 :
+# 3 
+# 4 
+# 6
+# Sample Output 1 :
+# 4.333333333333333
+# Sample Input 2 :
+# 5 
+# 10 
+# 5
+# Sample Output 2 :
+# 6.666666666666667
+
+
+
+# Read input as sepcified in the question
+# Print output as specified in the question
+f=int(input())
+s=int(input())
+t=int(input())
+avg=(f+s+t)/3
+print(avg)
